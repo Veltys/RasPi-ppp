e4c8eb772bd4500bfc8013851d2c1ed7c2f1cabd
Veltys <veltys@veltys.es>
2023-01-08 19:42:22 +0100

    ADD: Enable the traffic forwarding in server installation

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@
 # Description   : Install the components
 # Author        : Veltys
 # Date          : 2023-01-08
-# Version       : 1.0.0
+# Version       : 1.1.0
 # Usage         : sudo bash install.sh client / server | sudo ./install.sh client / server
 # Notes         :
 
@@ -18,4 +18,10 @@ else
 	systemctl daemon-reload
 	systemctl enable "pppd-${1}"
 	systemctl start "pppd-${1}"
+
+	if [ "$1" == 'server' ]; then
+		sysctl -w net.ipv4.ip_forward=1
+
+		iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
+	fi
 fi
